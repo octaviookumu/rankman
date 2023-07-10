ed92b11355e1f1304b1c2b1282d8e415f6d5ed6b
octaviookumu <octaviookumu@gmail.com>
2023-07-11 02:24:30 +0300

    Creating a NestJS Repository to Work with RedisJSON

--- server/src/polls/polls.service.ts
+++ server/src/polls/polls.service.ts
@@ -1,30 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { PollsRepository } from './polls.repository';
 import { CreatePollFields, JoinPollFields, RejoinPollFields } from './types';
 import { createPollID, createUserID } from './utils';
 
 @Injectable()
 export class PollsService {
+  private readonly logger = new Logger(PollsService.name);
+
+  constructor(private pollsRepository: PollsRepository) {}
+
   async createPoll(fields: CreatePollFields) {
     const pollID = createPollID();
     const userID = createUserID();
 
-    return {
+    const createdPoll = await this.pollsRepository.createPoll({
       ...fields,
-      userID,
       pollID,
+      userID,
+    });
+
+    return {
+      poll: createdPoll,
     };
   }
 
   async joinPoll(fields: JoinPollFields) {
     const userID = createUserID();
 
+    this.logger.debug(
+      `Fetching poll with ID: ${fields.pollID} for user with ID: ${userID}`,
+    );
+
+    const joinedPoll = await this.pollsRepository.getPoll(fields.pollID);
+
     return {
-      ...fields,
-      userID,
+      poll: joinedPoll,
     };
   }
 
   async rejoinPoll(fields: RejoinPollFields) {
-    return fields;
+    this.logger.debug(
+      `Rejoining poll with ID: ${fields.pollID} for user with ID: ${fields.userID} with name: ${fields.name}`,
+    );
+
+    const rejoinedPoll = await this.pollsRepository.addParticipant(fields);
+
+    return rejoinedPoll;
   }
 }
