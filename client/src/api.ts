2decb6571d4361e94872b1e3c39e6455adffa3be
octaviookumu <octaviookumu@gmail.com>
2023-08-29 23:06:09 +0300

    Set up Store

---
+++ client/src/api.ts
@@ -0,0 +1,59 @@
+const baseApiUrl = `http://${process.env.API_HOST}:${process.env.API_PORT}`;
+
+console.log("baseApiUrl", baseApiUrl);
+
+interface APIError {
+  messages: string[];
+  statusCode?: number;
+}
+
+interface MakeRequestResponse<T> {
+  data: T | Record<string, never>;
+  error?: APIError;
+}
+
+const makeRequest = async <T>(
+  endpoint: string,
+  reqInit?: RequestInit
+): Promise<MakeRequestResponse<T>> => {
+  try {
+    const response = await fetch(`${baseApiUrl}${endpoint}`, {
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      ...reqInit,
+    });
+
+    const responseJSON = await response.json();
+
+    if (!response.ok) {
+      return {
+        data: {},
+        error: responseJSON as APIError,
+      };
+    }
+
+    return {
+      data: responseJSON as T,
+    };
+  } catch (e) {
+    const error =
+      e instanceof Error
+        ? {
+            messages: [
+              process.env.MODE === "development" ? e.message : "Unknown error",
+            ],
+          }
+        : {
+            messages: ["Unknown error"],
+          };
+
+    return {
+      data: {},
+      error,
+    };
+  }
+};
+
+export { makeRequest };
---
+++ client/src/redux/features/poll-slice.ts
@@ -0,0 +1,67 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Poll } from "shared/poll-types";
+
+type InitialState = {
+  value: PollState;
+};
+
+type PollState = {
+  isLoading: boolean;
+  polls?: undefined | Poll;
+  accessToken: string;
+};
+
+const initialState = {
+  value: {
+    isLoading: false,
+    polls: undefined,
+  },
+} as InitialState;
+
+export const poll = createSlice({
+  name: "poll",
+  initialState,
+  reducers: {
+    startLoading: (state) => {
+      return {
+        value: {
+          isLoading: true,
+          polls: state.value.polls,
+          accessToken: state.value.accessToken,
+        },
+      };
+    },
+    stopLoading: (state) => {
+      return {
+        ...state,
+        value: {
+          isLoading: false,
+          polls: state.value.polls,
+          accessToken: state.value.accessToken,
+        },
+      };
+    },
+    initializePoll: (state, action: PayloadAction<Poll>) => {
+      return {
+        value: {
+          isLoading: false,
+          polls: action.payload,
+          accessToken: state.value.accessToken,
+        },
+      };
+    },
+    setAccessToken: (state, action: PayloadAction<string>) => {
+      return {
+        value: {
+          isLoading: false,
+          polls: state.value.polls,
+          accessToken: action.payload,
+        },
+      };
+    },
+  },
+});
+
+export const { startLoading, stopLoading, initializePoll, setAccessToken } =
+  poll.actions;
+export default poll.reducer;
