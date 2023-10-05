19e2eeb5e37b64a29543e774f3520d09bd517b50
octaviookumu <octaviookumu@gmail.com>
2023-10-05 20:18:49 +0300

    Lets see where this goes

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,13 +7,25 @@ import { VIABLE_URLS } from './common/constants';
 
 async function bootstrap() {
   const logger = new Logger('Main (main.ts)');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const configService = app.get(ConfigService);
   const port = parseInt(configService.get('PORT'));
   const clientPort = parseInt(configService.get('CLIENT_PORT'));
 
   console.log('VIABLE_URLS', VIABLE_URLS);
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
 
   app.enableCors();
   app.useWebSocketAdapter(new SocketIOAdapter(app, configService));
